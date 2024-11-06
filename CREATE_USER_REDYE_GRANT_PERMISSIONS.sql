6b5dfeda1d3535290a6a3c3151a4792cd3e0452c
Sneghashree13 <t.sn@northeastern.edu>
2024-11-06 15:52:32 -0500

    Add files via upload

---
+++ CREATE_USER_REDYE_GRANT_PERMISSIONS.sql
@@ -0,0 +1,41 @@
+-- Create User Block
+DECLARE
+    user_exists NUMBER;
+BEGIN
+    -- Check if the user 'REDEYE' exists in DBA_USERS
+    SELECT COUNT(*)
+    INTO user_exists
+    FROM dba_users
+    WHERE username = 'REDEYE';
+
+    -- Drop the user if it exists
+    IF user_exists > 0 THEN
+        EXECUTE IMMEDIATE 'DROP USER Redeye CASCADE';
+    END IF;
+
+    -- Create the user
+    EXECUTE IMMEDIATE 'CREATE USER Redeye IDENTIFIED BY NeuBoston2024#';
+END;
+/
+
+-- Grant Privileges to the user Redeye
+GRANT CREATE SESSION TO Redeye WITH ADMIN OPTION;
+GRANT CONNECT TO Redeye;
+ALTER USER Redeye QUOTA unlimited ON DATA;
+
+GRANT CREATE TABLE TO Redeye;
+GRANT CREATE VIEW TO Redeye;
+
+GRANT ALTER ANY TABLE TO Redeye;
+GRANT DROP ANY TABLE TO Redeye;
+
+GRANT SELECT ANY TABLE TO Redeye;
+GRANT INSERT ANY TABLE TO Redeye;
+GRANT UPDATE ANY TABLE TO Redeye;
+GRANT DELETE ANY TABLE TO Redeye;
+
+GRANT CREATE ANY INDEX TO Redeye;
+GRANT ALTER ANY INDEX TO Redeye;
+
+GRANT CREATE PROCEDURE TO Redeye;
+GRANT EXECUTE ANY PROCEDURE TO Redeye;
