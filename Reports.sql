065db86880bb94e326f9612205f486937637878c
sneghashree <mail2sneghashree@gmail.com>
2024-12-03 20:52:35 -0500

    updation of MaintenancePerMonth Report

--- Reports.sql
+++ Reports.sql
@@ -5,12 +5,14 @@ number of maintenance schedules conducted for shuttles on a monthly basis*/
 CREATE OR REPLACE VIEW MaintenancePerMonth AS
 SELECT
     TO_CHAR(maintenanceDate, 'YYYY-MM') AS maintenance_month,
+    shuttle_id,
     COUNT(*) AS maintenance_count
 FROM
     maintenance_schedules
 GROUP BY
-    TO_CHAR(maintenanceDate, 'YYYY-MM')
+    TO_CHAR(maintenanceDate, 'YYYY-MM'),
+    shuttle_id
 ORDER BY
-    maintenance_month;
+    maintenance_month, shuttle_id;
 
 
